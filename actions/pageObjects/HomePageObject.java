27907f0fe8cdbc23c47ba64faae5515e5a14f34c
Yuna Thuy Lieu <ngoclieu.ng@gmail.com>
2022-01-10 01:06:17 +0700

    Level_03_Page_Object_Login

--- actions/pageObjects/HomePageObject.java
+++ actions/pageObjects/HomePageObject.java
@@ -10,13 +10,28 @@ public class HomePageObject extends BasePage {
 	
 	public HomePageObject(WebDriver driver) {
 		this.driver = driver;
-		
 	}
 	
 	public void clickToRegisterLink() {
 		waitForElementClickable(driver, HomePageUI.REGISTER_LINK);
 		clickToElement(driver, HomePageUI.REGISTER_LINK);
+	}
+
+	public void clickToLoginLink() {
+		waitForElementClickable(driver, HomePageUI.REGISTER_LINK);
+		clickToElement(driver, HomePageUI.REGISTER_LINK);
 		
-	
-}
+	}
+
+	public boolean isMyAccountLinkDisplayed() {
+		waitForElementClickable(driver, HomePageUI.MY_ACCOUNT_LINK);
+		return isElementDisplayed(driver, HomePageUI.MY_ACCOUNT_LINK);
+	}
+
+	public void clickToMyAccountLink() {
+		waitForElementClickable(driver, HomePageUI.MY_ACCOUNT_LINK);
+		clickToElement(driver, HomePageUI.MY_ACCOUNT_LINK);
+		
+	}
+
 }
